a01e76069b1b1ba8cd7ece28e63ef1c01ce5df66
marekbily <bily1marek@gmail.com>
2024-11-21 11:10:49 +0100

    correct safeguards

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def fibonacci(n: int) -> int:
-    if n < 0:
+    if not isinstance(n, int) or n < 0:
         raise ValueError("Input must be a non-negative integer.")
     elif n == 0:
         return 0
