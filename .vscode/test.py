c3de738da6024f38b90c487eead410d5d8c30f0f
marekbily <bily1marek@gmail.com>
2024-11-21 10:49:40 +0100

    fibonacci add

---
+++ .vscode/test.py
@@ -0,0 +1,12 @@
+def fibonacci(n: int) -> int:
+    if n <= 0:
+        raise ValueError("Input must be a positive integer.")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(n - 2):
+            a, b = b, a + b
+        return b
